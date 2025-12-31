5121219bcbdeaba6d6a0a9754214dcc7d127a831
Mark Lindsay <godisawsume@icloud.com>
2025-12-31 10:44:40 -0800

    Create data-dailylogs.js

---
+++ data-dailylogs.js
@@ -0,0 +1,15 @@
+export const dailyLogs = {
+  "2025-12-31": {
+    bloodPressure: [
+      { systolic: 144, diastolic: 75, heartRate: 87, note: "Post strength" },
+      { systolic: 132, diastolic: 73, heartRate: 86, note: "Post treadmill 5 min" },
+      { systolic: 128, diastolic: 68, heartRate: 85, note: "Post recovery (baseline)" }
+    ],
+    glucose: [{ value: 5.4 }],
+    walk: 40,
+    treadmill: 10,
+    strength: 16,
+    calories: 12,
+    heartRate: 141
+  }
+};
