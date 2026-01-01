0419629dcaf85c86bbf94cd298f053042e72adc3
Mark Lindsay <godisawsume@icloud.com>
2026-01-01 11:52:48 -0800

    Create dailylogs.js

---
+++ dailylogs.js
@@ -0,0 +1,13 @@
+export const dailyLogs = {
+  "2026-01-01": {
+    bloodPressure: [
+      { systolic: 135, diastolic: 71, heartRate: 88, note: "5 min after strength training, high hypertension" }
+    ],
+    glucose: [], 
+    walk: 0,     
+    treadmill: 0,
+    strength: 10,
+    calories: 11,
+    heartRate: 88
+  }
+};
